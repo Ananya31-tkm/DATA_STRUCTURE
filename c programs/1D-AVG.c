de2379e807432a03cd5038e44e261a7b499e13b4
Ananya31-tkm <74641663+Ananya31-tkm@users.noreply.github.com>
2020-11-29 17:55:19 +0530

    arrays
    array

--- c programs/1D-AVG.c
+++ c programs/1D-AVG.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main()
 {
-     int marks[10], i, n, sum = 0, average;
+     float marks[10], i, n, sum = 0, average;
 
      printf("Enter number of elements: ");
-     scanf("%d", &n);
+     scanf("%f", &n);
 
      for(i=0; i<n; ++i)
      {
           printf("Enter number%d: ",i+1);
-          scanf("%d", &marks[i]);
+          scanf("%f", &marks[i]);
           
           // adding integers entered by the user to the sum variable
           sum += marks[i];
      }
 
      average = sum/n;
-     printf("Average = %d", average);
+     printf("Average = %f", average);
 
      return 0;
 }
---
+++ c programs/1d-min&max.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#define SIZE 10
+
+int main()
+{
+    int arr[SIZE] = {34,56,78,15,43,71,89,34,70,91};
+    int i, max, min;
+
+    max = min = arr[0];
+
+    for(i = 0; i < SIZE; i++)
+    {
+        // if value of current element is greater than previous value
+        // then assign new value to max
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        // if the value of current element is less than previous element
+        // then assign new value to min
+        if(my_arr[i] < min)
+        {
+            min = my_arr[i];
+        }
+     }
+
+    printf("Lowest value = %d\n", min);
+    printf("Highest value = %d", max);
+    return 0;
+}
